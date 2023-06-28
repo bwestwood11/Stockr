e919cc31992a06ccb22a15b3f6bbfbd1234e0c5a
Urbano <urbanobaz@yahoo.com>
2023-06-27 20:41:04 -0400

    Adding change for navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,17 @@
-'use client'
+"use client";
 
-import { useState } from "react"
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useState } from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Navbar() {
-   const { data: session } = useSession()
-   console.log(session)
+  const { data: session } = useSession();
+  console.log(session);
 
   return (
     <div>
-        <button onClick={() => signIn()}>Sign in with Google</button>
-        <button onClick={() => signOut()}>Sign out with Google</button>
+      <p>Navbar</p>
+      <button onClick={() => signIn()}>Sign in with Google</button>
+      <button onClick={() => signOut()}>Sign out with Google</button>
     </div>
-  )
+  );
 }
