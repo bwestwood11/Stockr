a4bddbfebf56c1e83870adc1bba7f204634e1c62
Urbano <urbanobaz@yahoo.com>
2023-07-06 10:42:25 -0400

    Adding changes for the table data in the gainers page

--- app/(site)/gainers/page.jsx
+++ app/(site)/gainers/page.jsx
@@ -1,12 +1,11 @@
 "use client";
 
-import { Suspense, use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
-import SearchStock from "@/components/SearchStock";
-import { useSearchParams } from "next/navigation";
 import { DataTable } from "./data-table";
 import { columns } from "./columns";
 import { useRouter } from "next/navigation";
+import { currencyFormat, percentFormat } from "@/lib/utils";
 
 export default function DashboardPage() {
   // const searchParams = useSearchParams();
@@ -30,15 +29,23 @@ export default function DashboardPage() {
   useEffect(() => {
     const getStocks = async () => {
       if (session?.user?.id) {
-        const response = await fetch(
-          `/api/gainers` );
+        const response = await fetch(`/api/gainers`);
         const data = await response.json();
         console.log("response", data);
-        setStocks(data.data.trends);
+        const updatedStocks = data.data.trends.map((obj) => {
+          const split = obj.symbol.split(":");
+          return {
+            ...obj,
+            symbol: split[0],
+            price: currencyFormat(obj.price),
+            change_percent: percentFormat(obj.change_percent),
+          };
+        });
+        setStocks(updatedStocks);
       }
     };
     getStocks();
-  }, [status])
+  }, [status]);
 
   // const handleDelete = async (id) => {
   //   const hasConfirmed = confirm(
@@ -50,7 +57,6 @@ export default function DashboardPage() {
   //       method: "DELETE",
   //     });
 
-
   //     const filteredStocks = stocks.filter((stock) => stock.id !== id);
   //     setStocks(filteredStocks);
   //   }
@@ -59,13 +65,15 @@ export default function DashboardPage() {
   return (
     <section className="text-center mt-20 w-full">
       <h1 className="mt-10 text-2xl font-extrabold leading-[1.15] text-black sm:text-6xl text-center">
-      {session?.user?.name ? `Welcome, ${session?.user?.name}` : "Welcome"}
-      <br />
-          <span className="bg-gradient-to-r from-green-400 to-green-500 bg-clip-text text-transparent text-center">Top Gainers</span>
-    </h1>
-    <p className="mt-5 text-lg text-gray-600 sm:text-xl max-w-2xl mx-auto text-center">
-      Checkout out yje top gainers below.
-    </p>
+        {session?.user?.name ? `Welcome, ${session?.user?.name}` : "Welcome"}
+        <br />
+        <span className="bg-gradient-to-r from-green-400 to-green-500 bg-clip-text text-transparent text-center">
+          Top Gainers
+        </span>
+      </h1>
+      <p className="mt-5 text-lg text-gray-600 sm:text-xl max-w-2xl mx-auto text-center">
+        Checkout out the top gainers below.
+      </p>
       {/* <Suspense fallback={<div className="text-center">Loading...</div>}>
         {stocks?.map((stock) => (
           <div key={stock._id}>
@@ -76,7 +84,7 @@ export default function DashboardPage() {
           </div>
         ))}
       </Suspense> */}
-        <DataTable columns={columns} data={stocks} />
+      <DataTable columns={columns} data={stocks} />
     </section>
   );
 }
